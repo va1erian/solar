663ed3dab6374e0bd1b058d53d5809394d0e9e2e
Valerian <smiloo@gmail.com>
2015-12-15 20:24:43 +0100

    fixed cam again and removed oops

--- app.js
+++ app.js
@@ -1528,64 +1528,6 @@ if (typeof module !== 'undefined') {
   });
 }
 
-;/* jshint ignore:start */
-'use strict';
-
-(function () {
-  var WebSocket = window.WebSocket || window.MozWebSocket;
-  var br = window.brunch = window.brunch || {};
-  var ar = br['auto-reload'] = br['auto-reload'] || {};
-  if (!WebSocket || ar.disabled) return;
-
-  var cacheBuster = function cacheBuster(url) {
-    var date = Math.round(Date.now() / 1000).toString();
-    url = url.replace(/(\&|\\?)cacheBuster=\d*/, '');
-    return url + (url.indexOf('?') >= 0 ? '&' : '?') + 'cacheBuster=' + date;
-  };
-
-  var browser = navigator.userAgent.toLowerCase();
-  var forceRepaint = ar.forceRepaint || browser.indexOf('chrome') > -1;
-
-  var reloaders = {
-    page: function page() {
-      window.location.reload(true);
-    },
-
-    stylesheet: function stylesheet() {
-      [].slice.call(document.querySelectorAll('link[rel=stylesheet]')).filter(function (link) {
-        var val = link.getAttribute('data-autoreload');
-        return link.href && val != 'false';
-      }).forEach(function (link) {
-        link.href = cacheBuster(link.href);
-      });
-
-      // Hack to force page repaint after 25ms.
-      if (forceRepaint) setTimeout(function () {
-        document.body.offsetHeight;
-      }, 25);
-    }
-  };
-  var port = ar.port || 9485;
-  var host = br.server || window.location.hostname || 'localhost';
-
-  var connect = function connect() {
-    var connection = new WebSocket('ws://' + host + ':' + port);
-    connection.onmessage = function (event) {
-      if (ar.disabled) return;
-      var message = event.data;
-      var reloader = reloaders[message] || reloaders.page;
-      reloader();
-    };
-    connection.onerror = function () {
-      if (connection.readyState) connection.close();
-    };
-    connection.onclose = function () {
-      window.setTimeout(connect, 1000);
-    };
-  };
-  connect();
-})();
-/* jshint ignore:end */
 ;require.register("js/components/BodyManipulator", function(exports, require, module) {
 'use strict';
 
@@ -1617,7 +1559,7 @@ var BodyManipulator = (function () {
 
 		window.addEventListener('mousedown', this.onMouseDown.bind(this), false);
 		window.addEventListener('mouseup', this.onMouseUp.bind(this), false);
-		window.addEventListener('keydown', this.onKeyDown.bind(this), false);
+		document.addEventListener('keydown', this.onKeyDown.bind(this), false);
 	}
 
 	_createClass(BodyManipulator, [{
@@ -1666,10 +1608,10 @@ var BodyManipulator = (function () {
 	}, {
 		key: 'onKeyDown',
 		value: function onKeyDown(event) {
-			switch (event.key) {
-				case 'p':
+			switch (event.keyCode) {
+				case 80:
 					exports.AnimationSpeed = AnimationSpeed += 0.5;return;
-				case 'o':
+				case 79:
 					exports.AnimationSpeed = AnimationSpeed -= 0.5;return;
 			}
 		}
